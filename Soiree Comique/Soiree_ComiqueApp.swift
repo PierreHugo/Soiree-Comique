9384272eeb852d6681485ccaca2aac8e567cdf31
PH <pierrehugo.herran@gmail.com>
2026-02-28 10:57:30 +0100

    initial code commit

---
+++ Soiree Comique/Soiree_ComiqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Soiree_ComiqueApp.swift
+//  Soiree Comique
+//
+//  Created by Pierre-Hugo Herran on 28/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Soiree_ComiqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
